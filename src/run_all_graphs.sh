1da78d322dad984cf079ba7205b1fe4eb4358688
Laurent Georget <laurent.georget@inria.fr>
2015-10-19 11:32:22 +0200

    Add two simple scripts to run all possible minimizations on dot graphs

---
+++ src/run_all_graphs.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+set -e
+set -x
+
+if [[ $# -lt 1 ]]
+then
+	echo "Usage: $0 <file.dot>"
+	exit 1
+fi
+
+dotfile=$1
+shift
+
+typeset -a transformations
+if [[ $# -ge 1 ]]
+then
+	transformations="$@"
+else
+	transformations=("taustar" "tauconfluence" "taudivergence" "strong"\
+	                 "safety"  "trace"         "weaktrace")
+fi
+
+./cleaner $dotfile
+if [[ ${dotfile:e} == "dot" ]]
+then
+	bcgfile=${dotfile/%dot/bcg}
+else
+	bcgfile="$dotfile.bcg"
+fi
+
+export CADP_CC="/usr/bin/gcc -fno-pie"
+for t in $transformations
+do
+	reducedbcgfile=${bcgfile/%.bcg/_$t.bcg}
+	bcg_open $bcgfile reductor -$t $reducedbcgfile
+	./grapher $reducedbcgfile
+	reduceddotfile=${reducedbcgfile/%bcg/dot}
+	dot -Tpng $reduceddotfile > ${reduceddotfile/%dot/png}
+done
+
---
+++ src/run_all_min.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+set -e
+set -x
+
+if [[ $# -lt 1 ]]
+then
+	echo "Usage: $0 <file.dot>"
+	exit 1
+fi
+
+dotfile=$1
+shift
+
+typeset -a transformations
+if [[ $# -ge 1 ]]
+then
+	transformations="$@"
+else
+	transformations=("branching" "divbranching" "observational")
+fi
+
+./cleaner $dotfile
+if [[ ${dotfile:e} == "dot" ]]
+then
+	bcgfile=${dotfile/%dot/bcg}
+else
+	bcgfile="$dotfile.bcg"
+fi
+
+export CADP_CC="/usr/bin/gcc -fno-pie"
+for t in $transformations
+do
+	reducedbcgfile=${bcgfile/%.bcg/_$t.bcg}
+	bcg_min -$t -normal $bcgfile $reducedbcgfile
+	./grapher $reducedbcgfile
+	reduceddotfile=${reducedbcgfile/%bcg/dot}
+	dot -Tpng $reduceddotfile > ${reduceddotfile/%dot/png}
+done
+
